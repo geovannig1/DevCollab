a97ff517c288c66112086b94244f55ff3fdcb02f
Lintang Digdoyo <digdoyo.lintang@gmail.com>
2021-02-25 19:09:50 +0700

    change static path

--- server/server.ts
+++ server/server.ts
@@ -59,10 +59,10 @@ app.use('/api/projects', meeting);
 //Serve static assets in productiion
 if (process.env.NODE_ENV === 'production') {
   //Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static(path.resolve(process.cwd(), 'client', 'build')));
 
   app.get('*', (req: Request, res: Response) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(process.cwd(), 'client', 'build', 'index.html'));
   });
 }
 
