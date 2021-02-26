fc1242d4f22bb12cf7baddd36afabbfab96ca8ad
Lintang Digdoyo <digdoyo.lintang@gmail.com>
2021-02-26 13:04:02 +0700

    remove service worker

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import store from './store';
 
